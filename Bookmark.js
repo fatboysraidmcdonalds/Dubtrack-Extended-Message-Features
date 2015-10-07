48cb6eec6f96bff4067521726deddc8676c1ac47
fatboysraidmcdonalds <joeyfas123@me.com>
2015-10-07 17:42:57 -0400

    Delete Bookmark.js

--- Bookmark.js
+++
@@ -1,4 +0,0 @@
-javascript:void((function(){$.get( "https://raw.githubusercontent.com/fatboysraidmcdonalds/Dubtrack-Message-Rules/master/Source.js", function( data ) {
-  var run = new Function(data);
-  run();
-});}()));
